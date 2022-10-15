4c988a79556fed76f4c2e68d1ec3c0a95a26bd56
DedSec-hun <55910572+DedSec-hun@users.noreply.github.com>
2022-10-15 19:25:44 +0200

    text

--- src/App.js
+++ src/App.js
@@ -35,59 +35,63 @@ function App() {
         </h1>
         <div class="overlay-content">
           <p>
-            Hello! I am doing Full Stack Software Development in an
-            Industry(4.0) environment and also managing a small developer team.
-            I am doing programming since my internship (2019), and
-            webdevelopment since I have a full time job (2020). A few example of
-            projects I worked on at my workplace:
+            I am doing Full Stack Software Development in an Industry(4.0)
+            environment and also managing a small developer team and its
+            projects. I am doing programming (C# console apps, services, wpf
+            apps) since my internship (2019), and web development since I have a
+            full time job (2020). A few example of projects I worked on at my
+            workplace:
             <br></br> <br></br>
             <li>
-              I designed and developed a Machine Change Management System from
-              scratch with Laravel which is used in the whole plant as a
-              plant-standard app. It's tracking changes made on the machines
-              around the factory and alerting stakeholders via email. Using
-              Azure authentication with 5 different user levels.
+              I designed and developed a Machine Change Management Tracking
+              System from scratch with Laravel which is now used in the whole
+              plant as a plant-standard app. It's tracking changes made on the
+              machines around the factory and alerting stakeholders via email.
+              Using Azure authentication, having a self made admin page and
+              connected to mysql db. Having data models, migrations, seeders, tests.
             </li>
             <li>
-              Changing a legacy .NET data collector app to Laravel framework and
-              redesign its database. Implement LDAP authentication.
+              Changing a legacy .NET Quality relevant data collector
+              app to Laravel PHP framework and redesign its database. Having LDAP
+              authentication.
             </li>
             <li>
               Made a material tracking system WPF app (together with my team)
               which is using a handheld barcode scanner to identify and handle
               materials stored in fridges, and store their expiration date and
-              other datas alert the user about events. Also showing thermometer
-              datas (Arduino,Wifi,Websocket) about the fridges.
+              other handling datas based on material type, alert the user about
+              events. Also showing thermometer datas (Arduino,Wifi,Websocket)
+              about the fridges.
             </li>
             <li>
               A DB Admin page, where my collegaues could modify a huge central
               database (it is driving IOT solutions) in a user friendly and safe
-              update environment with logs so nothing can go wrong.
+              update environment with logs and backups so nothing can go wrong.
             </li>
             <li>
-              Teamboard app: managing projects, todos, deadlines and workload of
+              Teamboard app: managing projects, info share, todos, deadlines and workload of
               team. Small react app with nodejs backend.
             </li>
             <li>
               My latest work is made with a React frontend and NodeJS express
-              backend with API calls. It is about showing who is available and
-              qualified to solve technical problems on the machines, and also to
-              connect these workers to specific production areas with changing
-              their roles on the frontend. If somebody called to a technical
-              problem an SMS is sent to him.
+              backend with API calls. It is about showing who is inside the
+              plant, available and qualified to solve technical (mechanical,
+              electrical, software) problems on the machines. If somebody called
+              to a technical problem an SMS is sent to him, reaction time,
+              problem solving time all calculated.
             </li>
             <li>
               SMS sender service written in C# using a GSM modul hardware. It
               can be used by many applications remotely. It is managed on a
               Windows server with the help of the Non-Sucking Service Manager.
-              It was developed in team.
+              It was developed in together wit a team member.
             </li>
             <li>
-              Data feeder APP which is collecting datas from multiple sources
-              after that recalculate them (mysql, Hadoop, Oracle, Webscraping)
-              then forwarding it to a plantwide used APP's database through API
-              calls. This app runs on Windows server called with a task
-              scheduler.
+              Data feeder fully OOP APP which is collecting datas from multiple
+              sources (mysql, Hadoop, Oracle, Webscraping, files on file
+              shares). After collection it recalculates them then forwards it to
+              a plantwide used APP's database through API calls. This app runs
+              on Windows server called with a task scheduler every day.
             </li>
           </p>
         </div>
@@ -100,7 +104,7 @@ function App() {
           <p>Hover on Icons to learn more!</p>
           <div>
             <img
-              data-tip="I know nodejs I used it mainly for APIs."
+              data-tip="I know nodejs I used it mainly for APIs and event-driven apps."
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/320px-Node.js_logo.svg.png"
               className="App-logo"
               alt="NodeJS"
@@ -112,19 +116,19 @@ function App() {
               alt="HTML"
             />
             <img
-              data-tip="This CV is using CSS with responsive design."
+              data-tip="This CV is using CSS with responsive design I just lack creativity."
               src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png"
               className="App-logo"
               alt="CSS"
             />
             <img
-              data-tip="I know how to create APIs with JS and PHP as well. And also use how to consume them."
+              data-tip="I know how to create APIs with JS and PHP as well, it drives everything. And also know use how to consume them."
               src="https://res.cloudinary.com/practicaldev/image/fetch/s--RK-AgEnh--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/1s3bedypkt7zm8maikzg.png"
               className="App-logo"
               alt="APIs"
             />
             <img
-              data-tip="I have a strong Javascript knowledge using it as vanilla js, react js, node js..."
+              data-tip="I have a strong Javascript knowledge using it as vanilla js, react js, node js and beginner level typescript..."
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png"
               className="App-logo"
               alt="Javascript"
@@ -132,25 +136,25 @@ function App() {
           </div>
           <div>
             <img
-              data-tip="This CV is made with React from different components and using hooks!"
+              data-tip="This CV is made with React!"
               src={logo}
               className="App-logo"
-              alt="logo"
+              alt="react"
             />
             <img
-              data-tip="This is my go to Framework with all its benefits. MVC architecture, OOP, Middlewares, APIs, Controllers, Testing..."
+              data-tip="This is my go to Framework in case of bigger projects, with all its benefits. MVC architecture, OOP, Middlewares, APIs, Controllers, Models, Testing..."
               src="https://png.pngitem.com/pimgs/s/578-5785080_laravel-6-logo-png-transparent-png.png"
               className="App-logo"
               alt="Laravel"
             />
             <img
-              data-tip="Good PHP knowledge needed for Laravel."
+              data-tip="A solid PHP knowledge is needed for Laravel."
               src="https://www.freepnglogos.com/uploads/logo-php-png/php-server-integration-wysiwyg-html-editor-froala-10.png"
               className="App-logo"
               alt="PHP"
             />
             <img
-              data-tip="Strong C# knowledge, ConsoleApps, WPF apps, Winform apps, Service apps, but not ASP.NET because there are better tools :)"
+              data-tip="I have a strong C# knowledge, ConsoleApps, WPF apps, Winform apps, Service apps, but not ASP.NET because there are better tools :)"
               src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png"
               className="App-logo"
               alt="C#"
@@ -158,7 +162,7 @@ function App() {
           </div>
           <div>
             <img
-              data-tip="I work with it every day"
+              data-tip="I work with it every day and it is the best db in my opinion"
               src="https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-images-are-download-crazypng-21.png"
               className="App-logo"
               alt="MYSQL"
@@ -184,7 +188,7 @@ function App() {
               alt="Unity"
             />
             <img
-              data-tip="I use it to create dashboards from multiple data sources."
+              data-tip="I use it to create dashboards from multiple data sources and to approach complicated data modeling needs."
               src="https://www.pei.com/wp-content/uploads/2016/08/maxresdefaultreduced.jpg"
               className="App-logo"
               alt="PowerBI"
@@ -197,7 +201,7 @@ function App() {
               alt="Windows Server / IIS"
             />
             <img
-              data-tip="Every project I did is tracked on GitHub."
+              data-tip="Every project I did is tracked on GitHub enterprise."
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1280px-Git-logo.svg.png"
               className="App-logo"
               alt="GIT"
